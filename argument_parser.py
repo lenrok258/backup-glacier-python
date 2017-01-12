ac5fd326214482fd4a6a09b58007739c350202a0
kornel.zemla <kornel.zemla@ocado.com>
2017-01-12 16:50:31 +0100

    All secrets as user input

--- argument_parser.py
+++ argument_parser.py
@@ -1,44 +1,56 @@
 import argparse
+import getpass
 import re
 
 
 class ArgumentName:
+    # User input
     AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID'
     AWS_SECRET_ACCESS_KEY = 'AWS_SECRET_ACCESS_KEY'
+    encryption_password = 'encryption_password'
+
+    # Script parameters
     AWS_REGION = 'AWS_REGION'
     AWS_GLACIER_VAULT = 'AWS_GLACIER_VAULT'
-    encryption_password = 'encryption_password'
     input_directory = 'input_directory'
     months_range = 'months_range'
 
 
 class ArgumentParser:
     def __init__(self):
+        self.__ask_for_secrets()
+
         parser = argparse.ArgumentParser(description='Backup media files to AWS Glacier')
-        parser.add_argument(ArgumentName.AWS_ACCESS_KEY_ID, help='Access key to access Amazon Glacier')
-        parser.add_argument(ArgumentName.AWS_SECRET_ACCESS_KEY, help='Secret key to access Amazon Glacier')
         parser.add_argument(ArgumentName.AWS_REGION, help='Region to access Amazon Glacier')
         parser.add_argument(ArgumentName.AWS_GLACIER_VAULT, help='Amazon Glacier vault name')
-        parser.add_argument(ArgumentName.encryption_password, help='Password to use to encrypt zip packages')
         parser.add_argument(ArgumentName.input_directory, help='Input directory')
         parser.add_argument(ArgumentName.months_range, type=ArgumentParser.months_arg_checker,
                             help='Months range, e.g. 1-6 or 7-12')
         self.args = parser.parse_args()
 
+    def __ask_for_secrets(self):
+        aws_key = getpass.getpass(ArgumentName.AWS_ACCESS_KEY_ID + ": ")
+        aws_secret = getpass.getpass(ArgumentName.AWS_SECRET_ACCESS_KEY + ": ")
+        enc_pass = getpass.getpass(ArgumentName.encryption_password + ": ")
+
+        setattr(self, ArgumentName.AWS_ACCESS_KEY_ID, aws_key)
+        setattr(self, ArgumentName.AWS_SECRET_ACCESS_KEY, aws_secret)
+        setattr(self, ArgumentName.encryption_password, enc_pass)
+
     def aws_key_id(self):
-        return getattr(self.args, ArgumentName.AWS_ACCESS_KEY_ID)
+        return getattr(self, ArgumentName.AWS_ACCESS_KEY_ID)
 
     def aws_secret(self):
-        return getattr(self.args, ArgumentName.AWS_SECRET_ACCESS_KEY)
+        return getattr(self, ArgumentName.AWS_SECRET_ACCESS_KEY)
+
+    def encryption_pass(self):
+        return getattr(self, ArgumentName.encryption_password)
 
     def aws_region(self):
         return getattr(self.args, ArgumentName.AWS_REGION)
 
     def aws_glacier_name(self):
         return getattr(self.args, ArgumentName.AWS_GLACIER_VAULT)
-
-    def encryption_pass(self):
-        return getattr(self.args, ArgumentName.encryption_password)
 
     def input_dir(self):
         return getattr(self.args, ArgumentName.input_directory)
