0382d92882fdad887c59c39b49f0b49f19e212a0
kornel.zemla <kornel.zemla@ocado.com>
2017-01-16 17:07:09 +0100

    Glacer multipart upload added

--- aws_glacier.py
+++ aws_glacier.py
@@ -1,24 +1,32 @@
 import os
 
+import boto.glacier.layer2 as boto_glacier
 import boto3
 
 
 def upload_files(file_paths_list, aws_key, aws_secret, aws_region, aws_glacier_vault):
     client = __create_glacier_client(aws_key, aws_region, aws_secret)
+    layer2 = __create_glacier_layer2()
     __set_retrieval_policy_to_free_tier_only(client)
 
     for file_path in file_paths_list:
-        with open(file_path, 'rb') as file:
-            print "About to upload file [{}]".format(file.name)
-            __upload_file(client, file, aws_glacier_vault)
+        print "About to upload file [{}]".format(file_path)
+        archive_id = __upload_file(layer2, file_path, aws_glacier_vault)
+        print "File uploaded. Archive id = {}".format(archive_id)
 
 
-def __create_glacier_client(aws_key, aws_region, aws_secret):
-    client = boto3.client('glacier',
-                          region_name=aws_region,
-                          aws_access_key_id=aws_key,
-                          aws_secret_access_key=aws_secret)
+def __create_glacier_client(aws_key, aws_secret, aws_region):
+    client = boto3.client(aws_access_key_id=aws_key,
+                          aws_secret_access_key=aws_secret,
+                          region_name=aws_region)
     return client
+
+
+def __create_glacier_layer2(aws_key, aws_secret, aws_region):
+    layer2 = boto_glacier.Layer2(aws_access_key_id=aws_key,
+                                 aws_secret_access_key=aws_secret,
+                                 region_name=aws_region)
+    return layer2
 
 
 def __set_retrieval_policy_to_free_tier_only(client):
@@ -31,7 +39,7 @@ def __set_retrieval_policy_to_free_tier_only(client):
     })
 
 
-def __upload_file(client, file, aws_glacier_vault):
-    client.upload_archive(vaultName=aws_glacier_vault,
-                          archiveDescription=os.path.basename(file.name),
-                          body=file)
+def __upload_file(layer2, file_path, aws_glacier_vault):
+    vault = layer2.get_vault(aws_glacier_vault)
+    description = os.path.basename(file_path)
+    return vault.upload_archive(file_path, description)
